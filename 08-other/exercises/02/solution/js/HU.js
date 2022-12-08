9359b96dc6154961c2f7fed3d81105a990539b7b
timivarga <timea.varga252@gmail.com>
2022-12-08 11:26:57 +0100

    kesz

--- 08-other/exercises/02/solution/js/HU.js
+++ 08-other/exercises/02/solution/js/HU.js
@@ -1,1 +1,22 @@
-export default HU;
+// 2. Készíts egy `HU` nevű objektumot, aminek az alábbi propertijei vannak:
+// - `date()`: Visszaadja a paraméterként megadott dátumot a magyar dátumírás szabályainak megfelelően
+// - `currency()`: Visszaadja a paraméterként megadott számot a magyar pénzformátumnak megfelelően a Ft végződéssel együtt
+// - `list()`: A paraméterként kapott string tömböt visszaadja az alábbi formában: első, második és harmadik" (Tehát vesszőkkel elválasztva az utolsó elem előtt az és szóval)
+
+const HU = {
+    date(date) {
+      return new Intl.DateTimeFormat('hu-HU').format(date);
+    },
+    currency(price) {
+      return new Intl.NumberFormat('hu-HU', {
+        style: 'currency',
+        currency: 'HUF',
+      }).format(price);
+    },
+    list(arr) {
+      const lastElement = arr.pop();
+      return arr.join(', ').concat(` és ${lastElement}`);
+    },
+  };
+  
+  export default HU;
